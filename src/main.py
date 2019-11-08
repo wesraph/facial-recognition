b97a456410acb486274186c2c0225fef986f2041
Raphael Westphal <westphal.raphael@gmail.com>
2019-11-08 14:31:03 +0100

    pouet

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from os import listdir
 from os.path import isfile, join
 from sklearn import preprocessing
+import os
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import numpy as np
@@ -16,6 +17,10 @@ DATASET_DIR_NEGATIVE="../data/dataset1/negative/"
 
 def loadImageToArray(path):
     jpg_list = [f for f in listdir(path) if isfile(join(path, f))]
+
+    if os.name == 'nt':
+        if jpg_list[0] == "desktop.ini":
+            jpg_list.pop(0)
 
     if(len(jpg_list) == 0):
         print("No files found at " + path)
@@ -33,14 +38,15 @@ def loadImageToArray(path):
 
 def findBestR(model, isRandom=False, limit=100):
     print("Computing average R")
-    gallery = m["gallery"]
-    averageVector = np.mean(gallery, axis=0)
-    distances = search.compute_distances(gallery, averageVector)
-    averageR = np.sum(distances) / len(distances)
-    print("Average R:", averageR)
+    print("lenGallery", len(m["gallery"]))
 
+    gallery = m["gallery"]
+    lenGallery =len(m["gallery"][0])
+    #origin = np.zeros((1, lenGallery))
+    #distances = search.compute_distances(gallery, origin)
+    #averageR = np.sum(distances) / len(distances)
+    #print("Average R:", averageR/1000000000)
     r = 0
-    lenGallery = len(gallery)
     iterator = []
 
     if isRandom:
@@ -48,22 +54,25 @@ def findBestR(model, isRandom=False, limit=100):
     else:
         iterator = range(0, lenGallery)
 
-    print(iterator)
     lenIterator = len(iterator)
 
     u = 0
+
+    dmax = []
+
     for i in iterator:
         print((u / lenIterator * 100))
         u = u + 1
-
-        sliced = np.concatenate([gallery[:i],gallery[i+1:]])
-
+        sliced = np.concatenate((gallery[:i],gallery[i+1:]), axis=0)
         distances = search.compute_distances(sliced, gallery[i])
-        dmax = np.amin(distances)
-        if dmax > r and dmax < 2 * averageR:
-            r = dmax
+        min = np.amin(distances)
+        dmax.append(min)
 
-    return r
+    dmax = np.array(dmax)
+    average = np.mean(dmax)
+    sorted =  dmax[np.where(dmax < 2*average)]
+    bestR = np.amax(sorted)
+    return bestR
 
 def transformDataset(data, eigenFaces, averageVector):
     data = np.array(data)
@@ -96,7 +105,10 @@ def evaluateRadius(gallery, posProbes, negProbes, r):
     print("Accepted probes: ", acceptedProbes / (lenPosProbes + lenNegProbes))
     print("False accepted probes: ", falseAcceptedProbes / (lenPosProbes + lenNegProbes))
     print("Refused probes: ", refusedProbes / (lenPosProbes + lenNegProbes))
-    print("Accepted probes: ", falseRefusedProbes / (lenPosProbes + lenNegProbes))
+    print("False refused probes: ", falseRefusedProbes / (lenPosProbes + lenNegProbes))
+
+    globalEfficiency = (acceptedProbes / (lenPosProbes + lenNegProbes)) + (refusedProbes / (lenPosProbes + lenNegProbes))
+    return globalEfficiency
 
 def applyPCA(data):
     print("Compute average vector")
@@ -112,12 +124,27 @@ def applyPCA(data):
 
     print("Computing eigen(vector|values)")
     eigenValues, eigenVectors = np.linalg.eigh(covMat)
-    eigenFaces = data.T.dot(eigenVectors)
-    eigenFaces = preprocessing.normalize(eigenFaces)
+    eigenVectors = data.T.dot(eigenVectors)
+    eigenFaces = preprocessing.normalize(eigenVectors)
 
     eigenValues = eigenValues * ((d - 1) / (n - 1))
 
     return eigenFaces, eigenValues, averageVector
+
+
+def reduceSpaces(gallery, eigenValues, n=10):
+    indices = np.flip(np.argsort(eigenValues))
+
+    print(indices)
+
+    newGallery = []
+    for i in range(0, len(gallery)):
+        el = []
+        for u in range(0, n):
+            el.append(gallery[i][indices[u]])
+        newGallery.append(np.array(el))
+
+    return np.array(newGallery)
 
 def importDataset(path):
     data = np.load(path).tolist()
@@ -156,21 +183,44 @@ def loadModel(path):
     with open(path, "rb") as f:
         return pickle.load(f)
 
-def transformGalleryAndSave(path, eigenFaces,):
-    print("Loading gallery")
-    gallery = loadImageToArray(path)
-    print("Transforming gallery")
-    gallery = transformDataset(gallery, eigenFaces, averageVector)
-
 def loadAndTransform(path, m):
     data = loadImageToArray(path)
     return transformDataset(data, m['eigenFaces'], m['averageVector'])
 
+def settingsImpact(m):
+    print("Loading and transform posProbes")
+    posProbes = loadAndTransform(DATASET_DIR_POSITIVE, m)
+    print("Loading and transform negProves")
+    negProbes = loadAndTransform(DATASET_DIR_NEGATIVE, m)
+
+    m["gallery"] = reduceSpaces(m["gallery"], m["eigenValues"])
+    posProbes = reduceSpaces(posProbes, m["eigenValues"])
+    negProbes = reduceSpaces(negProbes, m["eigenValues"])
+
+    print(len(m["gallery"]))
+    print("reduced gallery length", len(m["gallery"][0]))
+    print("reduced posProbes length", len(posProbes[0]))
+    print("reduced negProbes length", len(negProbes[0]))
+
+    r = findBestR(m, isRandom=True, limit=200)
+    print(r)
+
+    percentageRange = np.arange(0, 0.5, 0.05)
+    efficiencyAxis = []
+    for percent in percentageRange:
+        print( r + r*percent)
+        efficiency = evaluateRadius(m["gallery"], posProbes, negProbes, r + r*percent)
+        print(efficiency)
+        efficiencyAxis.append(efficiency)
+        print(percent*100, "%")
+    plt.plot(percentageRange*100, efficiencyAxis)
+
+# def reduceSpaces(m, posProbes, negProbes, nbEF = 20):
+        # return [i[:nbEF] for i in m["gallery"]],[i[:nbEF] for i in posProbes],[i[:nbEF] for i in negProbes]
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-a", "--action", type=str,
-                    help="[plotEigenValues, generateModel, findBestR, createRawModel, perfCompare, evaluateRadius]")
-
-
+                    help="[generateModel, findBestR, evaluateRadius]")
 
 args = parser.parse_args()
 if args.action == "plotEigenValues":
@@ -184,7 +234,11 @@ if args.action == "plotEigenValues":
     print(ev)
     plt.show()
 
-if args.action == "generateModel":
+elif args.action == "settingsImpact":
+    m = loadModel("model.pkl")
+    settingsImpact(m)
+
+elif args.action == "generateModel":
     print("Generating model")
     trainModelAndSave(DATASET_DIR_1)
 
@@ -199,15 +253,6 @@ elif args.action == "findBestR":
     print("Updating model")
     m["r"] = bestR
     saveModel(m)
-
-elif args.action == "createRawModel":
-    print("Creating raw model (no optimisation)")
-    m  = {}
-    m["gallery"] = loadImageToArray(DATASET_DIR_1)
-    m["r"] = findBestR(m, isRandom=True, limit=200)
-
-    print("Saving model")
-    saveModel("rawModel.pkl")
 
 elif args.action == "evaluateRadius":
     print("Loading model")
